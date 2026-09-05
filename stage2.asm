6e9d62d47b4b2b19341ac20ea0f75e670cc816f6
unknown <you@example.com>
2026-09-05 11:19:48 +0800

    Update 2026-09-05 11:19:48

--- stage2.asm
+++ stage2.asm
@@ -9,11 +9,11 @@ start:
     mov ds, ax
     mov es, ax
     
-    ; Copy font data to 0x9000 for reliable access
-    ; DS=0x0000, SI=font_8x8 (absolute address 0x8033)
-    ; ES=0x0000, DI=0x9000
+    ; Copy font data to 0x8000 for reliable access
+    ; DS=0x0000, SI=font_8x8 (absolute address)
+    ; ES=0x0000, DI=0x8000
     mov si, font_8x8
-    mov di, 0x9000
+    mov di, 0x8000
     mov cx, 768          ; 96 characters * 8 bytes = 768 bytes
     cld
     rep movsb
@@ -337,9 +337,9 @@ draw_char_8x8:
     mov bx, ax
     shl bx, 3            ; BX = (char - 0x20) * 8
     
-    ; SI = 0x9000 + offset (font data copied here at startup)
-    mov si, 0x9000
-    add si, bx           ; SI = 0x9000 + (char - 0x20) * 8
+    ; SI = 0x8000 + offset (font data copied here at startup)
+    mov si, 0x8000
+    add si, bx           ; SI = 0x8000 + (char - 0x20) * 8
     
     ; Set ES to VRAM for drawing
     mov ax, 0xA000
@@ -349,8 +349,8 @@ draw_char_8x8:
     mov cx, 8
     mov [char_row], cx
 draw_char_row:
-    ; Get font byte for this row from code segment using CS prefix
-    mov al, [cs:si]
+    ; Get font byte for this row from 0x8000
+    mov al, [si]
     inc si
     
     ; Calculate current Y position
