4703ad23068c0be957ed34ced641c501eef1fa89
unknown <you@example.com>
2026-09-05 11:11:14 +0800

    Update 2026-09-05 11:11:14

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ call "%~dp0build.bat"
 if errorlevel 1 exit /b 1
 
 :: Start QEMU with vmware VGA
-start "" "%~dp0..\Program\qemu\qemu-system-x86_64.exe" -hda "%~dp0build\nova-os.img" -m 32M -boot c -vga vmware
+"%~dp0..\Program\qemu\qemu-system-x86_64.exe" -hda "%~dp0build\nova-os.img" -m 32M -boot c -vga vmware
